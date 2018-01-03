693d2a18d63e5a0c4d4926e0adca59ae55c0e63d
Josh Karpel <josh.karpel@gmail.com>
2018-01-02 22:09:48 -0600

    Ask user which problem to solve at the command line

--- euler/src/ProjectEuler.java
+++ euler/src/ProjectEuler.java
@@ -1,13 +1,17 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class ProjectEuler {
     public static void main(String[] args) {
-        Map<String, solvers.Solver> solverMap = getSolverMap();
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Which problem would you like to solve?   ");
+        String which_solver = String.format("%03d", reader.nextInt());
 
-        solvers.Solver solver = solverMap.get(args[0]);
+        Map<String, solvers.Solver> solverMap = getSolverMap();
+        solvers.Solver solver = solverMap.get(which_solver);
         if (solver == null) {
-            System.out.println("Couldn't find a solver for problem " + args[0]);
+            System.out.println("Couldn't find a solver for problem " + which_solver);
             System.exit(0);
         }
         System.out.println("Answer: " + solver.solve());
